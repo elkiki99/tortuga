108a7ab04de9bf62ae2a454cc4dd8e8b7eebbd61
elkiki99 <brossani23@gmail.com>
2025-07-07 12:15:07 -0300

    preparing to deploy 5.0

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -8,15 +8,14 @@ use App\Models\Category;
 class CategorySeeder extends Seeder
 {
     /**
-    * Run the database seeds.
-    */
+     * Run the database seeds.
+     */
     public function run(): void
     {
         $parents = Category::factory(6)->create();
 
-        Category::factory(12)->make()->each(function ($child) use ($parents) {
-            $child->parent_id = $parents->random()->id;
-            $child->save();
-        });
+        Category::factory(12)->create([
+            'parent_id' => $parents->random()->id
+        ]);
     }
 }
